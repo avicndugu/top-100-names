081eec9a8efc790b2a39b261b41f1bf7aa47af9f
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-05-19 13:43:05 +0300

    Restructuring data using tables

--- src/App.js
+++ src/App.js
@@ -14,23 +14,23 @@ function App() {
           <h1>Top 100 Baby Names in 2021</h1>
           <div className="Row">
             <div className="Column">
-              <ol>
-                <li>NAME</li>
-                <li>NAME</li>
-                <li>NAME</li>
-                <li>NAME</li>
-                <li>NAME</li>
-              </ol>
+              <table>
+                <tr><td>1. NAME</td></tr>
+                <tr><td>2. NAME</td></tr>
+                <tr><td>3. NAME</td></tr>
+                <tr><td>4. NAME</td></tr>
+                <tr><td>5. NAME</td></tr>
+              </table>
               <a href="#">View All</a>
             </div>
             <div className="Column">
-              <ol>
-                <li>NAME</li>
-                <li>NAME</li>
-                <li>NAME</li>
-                <li>NAME</li>
-                <li>NAME</li>
-              </ol>
+              <table>
+                <tr><td>1. NAME</td></tr>
+                <tr><td>2. NAME</td></tr>
+                <tr><td>3. NAME</td></tr>
+                <tr><td>4. NAME</td></tr>
+                <tr><td>5. NAME</td></tr>
+              </table>
               <a href="/year/gender/">View All</a>
             </div>
           </div>
@@ -38,29 +38,49 @@ function App() {
         <div className="Row">
           <div className="Column">
             <h2>Top 100 Baby Boys Names in 2021</h2>
-            <ol>
-              <li>NAME</li>
-              <li>NAME</li>
-              <li>NAME</li>
-              <li>NAME</li>
-              <li>NAME</li>
-            </ol>
+            <table>
+              <tr><td>1. NAME</td></tr>
+              <tr><td>2. NAME</td></tr>
+              <tr><td>3. NAME</td></tr>
+              <tr><td>4. NAME</td></tr>
+              <tr><td>5. NAME</td></tr>
+            </table>
               <a href="/years/gender/">View All</a>
           </div>
           <div className="Column">
             <h2>Top 100 Baby Girls Names in 2021</h2>
-            <ol>
-              <li>NAME</li>
-              <li>NAME</li>
-              <li>NAME</li>
-              <li>NAME</li>
-              <li>NAME</li>
-            </ol>
+            <table>
+              <tr><td>1. NAME</td></tr>
+              <tr><td>2. NAME</td></tr>
+              <tr><td>3. NAME</td></tr>
+              <tr><td>4. NAME</td></tr>
+              <tr><td>5. NAME</td></tr>
+            </table>
               <a href="/year/gender/">View All</a>
           </div>
         </div>
         <div>
           <h2>Top 100 Baby Names By Year</h2>
+          <ul>
+            <li><a href="/year/">NAME</a></li>
+            <li><a href="/year/">NAME</a></li>
+            <li><a href="/year/">NAME</a></li>
+            <li><a href="/year/">NAME</a></li>
+            <li><a href="/year/">NAME</a></li>
+          </ul>
+        </div>
+        <div>
+          <h2>Top 100 Girls Names By Year</h2>
+          <ul>
+            <li><a href="/year/">NAME</a></li>
+            <li><a href="/year/">NAME</a></li>
+            <li><a href="/year/">NAME</a></li>
+            <li><a href="/year/">NAME</a></li>
+            <li><a href="/year/">NAME</a></li>
+          </ul>
+        </div>
+        <div>
+          <h2>Top 100 Boys Names By Year</h2>
           <ul>
             <li><a href="/year/">NAME</a></li>
             <li><a href="/year/">NAME</a></li>
