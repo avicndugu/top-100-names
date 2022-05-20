0ae60d0f6d97c2c3c592c78ee447682c9c3618af
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-05-20 12:31:39 +0300

    Enhance table markup

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,30 +8,34 @@ function Home() {
         <div className="Row">
           <div className="Column">
             <table>
-              <tr><td>1. NAME</td></tr>
-              <tr><td>2. NAME</td></tr>
-              <tr><td>3. NAME</td></tr>
-              <tr><td>4. NAME</td></tr>
-              <tr><td>5. NAME</td></tr>
-              <tr><td>6. NAME</td></tr>
-              <tr><td>7. NAME</td></tr>
-              <tr><td>8. NAME</td></tr>
-              <tr><td>9. NAME</td></tr>
-              <tr><td>10. NAME</td></tr>
+              <tbody>
+                <tr><td>1. NAME</td></tr>
+                <tr><td>2. NAME</td></tr>
+                <tr><td>3. NAME</td></tr>
+                <tr><td>4. NAME</td></tr>
+                <tr><td>5. NAME</td></tr>
+                <tr><td>6. NAME</td></tr>
+                <tr><td>7. NAME</td></tr>
+                <tr><td>8. NAME</td></tr>
+                <tr><td>9. NAME</td></tr>
+                <tr><td>10. NAME</td></tr>
+              </tbody>
             </table>
           </div>
           <div className="Column">
             <table>
-              <tr><td>11. NAME</td></tr>
-              <tr><td>12. NAME</td></tr>
-              <tr><td>13. NAME</td></tr>
-              <tr><td>14. NAME</td></tr>
-              <tr><td>15. NAME</td></tr>
-              <tr><td>16. NAME</td></tr>
-              <tr><td>17. NAME</td></tr>
-              <tr><td>18. NAME</td></tr>
-              <tr><td>19. NAME</td></tr>
-              <tr><td>20. NAME</td></tr>
+              <tbody>
+                <tr><td>11. NAME</td></tr>
+                <tr><td>12. NAME</td></tr>
+                <tr><td>13. NAME</td></tr>
+                <tr><td>14. NAME</td></tr>
+                <tr><td>15. NAME</td></tr>
+                <tr><td>16. NAME</td></tr>
+                <tr><td>17. NAME</td></tr>
+                <tr><td>18. NAME</td></tr>
+                <tr><td>19. NAME</td></tr>
+                <tr><td>20. NAME</td></tr>
+              </tbody>
             </table>
             <a href="/year/gender/">View All</a>
           </div>
@@ -41,22 +45,27 @@ function Home() {
         <div className="Column">
           <h2>Top 100 Baby Boys Names in 2021</h2>
           <table>
-            <tr><td>1. NAME</td></tr>
-            <tr><td>2. NAME</td></tr>
-            <tr><td>3. NAME</td></tr>
-            <tr><td>4. NAME</td></tr>
-            <tr><td>5. NAME</td></tr>
+            <tbody>
+              <tr><td>1. NAME</td></tr>
+              <tr><td>2. NAME</td></tr>
+              <tr><td>3. NAME</td></tr>
+              <tr><td>4. NAME</td></tr>
+              <tr><td>5. NAME</td></tr>
+              <tr><td>5. NAME</td></tr>
+            </tbody>
           </table>
             <a href="/years/gender/">View All</a>
         </div>
         <div className="Column">
           <h2>Top 100 Baby Girls Names in 2021</h2>
           <table>
-            <tr><td>1. NAME</td></tr>
-            <tr><td>2. NAME</td></tr>
-            <tr><td>3. NAME</td></tr>
-            <tr><td>4. NAME</td></tr>
-            <tr><td>5. NAME</td></tr>
+            <tbody>
+              <tr><td>1. NAME</td></tr>
+              <tr><td>2. NAME</td></tr>
+              <tr><td>3. NAME</td></tr>
+              <tr><td>4. NAME</td></tr>
+              <tr><td>5. NAME</td></tr>
+            </tbody>
           </table>
             <a href="/year/gender/">View All</a>
         </div>
