617f545e0e1e4b7a9267ee3f72c5b06d1107fb12
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-05-20 13:15:48 +0300

    Fetch data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,48 +1,122 @@
 import YearLinks from '../../components/YearLinks';
 import TopNames from '../../components/TopNames';
+import {useState, useEffect} from "react";
 
+function useFetch(url) {
+  const [loadingData, setLoadingData] = useState(false);
+  const [data, setData] = useState(null);
+  useEffect(() => { 
+    const fetchData = async() => {
+      try{
+        setLoadingData(true);
+        fetch(url)
+          .then(response => response.json())
+          .then((data) => { setData(data) })
+        setLoadingData(false);
+      } catch(error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  },[]);
+  return[loadingData, data];
+}
 
 function Home() {
-  return (
-    <>
-      <div>
-        <h1>Top 100 Baby Names in 2021</h1>
+  const [isDataLoading, data] = useFetch('https://avicndugu.github.io/top-100-names/yob2021.json');
+
+  if (isDataLoading || !data){
+    return (
+      <>
+        <div>
+          <h1>Top 100 Baby Names in 2021</h1>
+          <div className="Row">
+            <div className="Column">
+              <p>Data is loading...</p>
+            </div>
+            <div className="Column">
+              <p>Data is loading...</p>
+            </div>
+          </div>
+        </div>
         <div className="Row">
           <div className="Column">
-            <TopNames />
+            <h2>Top 100 Baby Boys Names in 2021</h2>
+            <p>Data is loading...</p>           
+              <a href="/years/gender/">View All</a>
           </div>
           <div className="Column">
-            <TopNames />
+            <h2>Top 100 Baby Girls Names in 2021</h2>
+            <p>Data is loading...</p>
             <a href="/year/gender/">View All</a>
           </div>
         </div>
-      </div>
-      <div className="Row">
-        <div className="Column">
-          <h2>Top 100 Baby Boys Names in 2021</h2>
-          <TopNames />            
-            <a href="/years/gender/">View All</a>
+        <div>
+          <h2>Top 100 Baby Names By Year</h2>
+          <YearLinks />
         </div>
-        <div className="Column">
-          <h2>Top 100 Baby Girls Names in 2021</h2>
-          <TopNames />
-          <a href="/year/gender/">View All</a>
+        <div>
+          <h2>Top 100 Girls Names By Year</h2>
+          <YearLinks />
         </div>
-      </div>
-      <div>
-        <h2>Top 100 Baby Names By Year</h2>
-        <YearLinks />
-      </div>
-      <div>
-        <h2>Top 100 Girls Names By Year</h2>
-        <YearLinks />
-      </div>
-      <div>
-        <h2>Top 100 Boys Names By Year</h2>
-        <YearLinks />
-      </div>
-    </>
-  );
+        <div>
+          <h2>Top 100 Boys Names By Year</h2>
+          <YearLinks />
+        </div>
+      </>
+    )
+  } else {
+    console.log(data)
+    console.log(data[0].name)
+    return (
+      <>
+        <div>
+          <h1>Top 100 Baby Names in 2021</h1>
+          <div className="Row">
+            <div className="Column">
+              {data.map((item) => (
+                <p>{item.name}</p>
+              ))}
+              <TopNames name = { data } />
+            </div>
+            <div className="Column">
+              {data.map((item) => (
+                <p>{item.name}</p>
+              ))}
+              <TopNames names = { data }/>
+              <a href="/year/gender/">View All</a>
+            </div>
+          </div>
+        </div>
+        <div className="Row">
+          <div className="Column">
+            <h2>Top 100 Baby Boys Names in 2021</h2>
+            <TopNames name = { data }/>            
+              <a href="/years/gender/">View All</a>
+          </div>
+          <div className="Column">
+            <h2>Top 100 Baby Girls Names in 2021</h2>
+            <TopNames name = { data }/>
+            <a href="/year/gender/">View All</a>
+          </div>
+        </div>
+        <div>
+          <h2>Top 100 Baby Names By Year</h2>
+          <YearLinks />
+        </div>
+        <div>
+          <h2>Top 100 Girls Names By Year</h2>
+          <YearLinks />
+        </div>
+        <div>
+          <h2>Top 100 Boys Names By Year</h2>
+          <YearLinks />
+        </div>
+      </>
+    );
+
+  }
 }
+
 
 export default Home;
