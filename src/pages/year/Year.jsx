7aac2ce6571d200936071cc5acc1266596c9d972
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-05-27 10:41:49 +0300

    Align Year page to Top page data requirements

--- src/pages/year/Year.jsx
+++ src/pages/year/Year.jsx
@@ -28,6 +28,9 @@ function useFetch(url) {
 function Year() {
   let params = useParams();
   const [isDataLoading, data] = useFetch(`https://avicndugu.github.io/top-100-names/yob${params.year}.json`);
+  const [seeAllGirls, setSeeAllGirls] = useState(false);
+  const [seeAllBoys, setSeeAllBoys] = useState(false);
+
   if (isDataLoading || !data){
     return (
       <>
@@ -46,6 +49,29 @@ function Year() {
     )
   } else{
 
+    const newArrGirls= [];
+    const newArrBoys= [];
+
+    const girls = data.filter((name) => name.gender === "F");
+    girls.forEach((item, index) =>{
+      item.pos = index + 1;
+      newArrGirls.push(item)
+      return newArrGirls;
+    })
+
+
+    const boys = data.filter((name) => name.gender === "M");
+    boys.forEach((item, index) =>{
+      item.pos = index + 1;
+      newArrBoys.push(item)
+      return newArrBoys;
+    })
+
+    const top10girls=girls.filter(name => name.pos <= 10);
+    const top10boys=boys.filter(name => name.pos <= 10);
+    const top100girls=girls.filter(name => name.pos <= 100);
+    const top100boys=boys.filter(name => name.pos <= 100);
+
     return (
       <>
         <div>
@@ -55,11 +81,21 @@ function Year() {
         <div className="Row">
           <div className="Column">
             <h2>Top 100 Baby Boys Names in { params.year }</h2>
-            <TopNames names = { data.filter((name) => name.gender === "M") } gender = "m" />
+            <table>
+              <tbody>
+                <TopNames top10 ={ top10boys }  top100={ top100boys } gender = "m" seeall={ seeAllBoys } />
+              </tbody>
+            </table>
+            <button onClick={()=> setSeeAllBoys(true)}>View All</button>
           </div>
           <div className="Column">
             <h2>Top 100 Baby Girls Names in { params.year }</h2>
-            <TopNames names = { data.filter((name) => name.gender === "F") } gender = "m" />            
+            <table>
+              <tbody>
+                <TopNames top10 ={ top10girls } top100={ top100girls } gender = "f" seeall={ seeAllGirls }/>
+              </tbody>
+            </table>
+            <button onClick={()=> setSeeAllGirls(true)}>View All</button>
           </div>
         </div>
         <div>
