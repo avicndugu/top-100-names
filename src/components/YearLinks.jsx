9be7c6bd2a4b82bdb2cf6a8bc4b96b3ca4c99886
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-05-31 17:25:22 +0300

    Update bottom links to match correct value

--- src/components/YearLinks.jsx
+++ src/components/YearLinks.jsx
@@ -2,11 +2,29 @@ import { Link } from 'react-router-dom';
 
 const YearLinks = (props) => {
 let years = [2021, 2020, 2019, 2018];
-  return (
-    years.map((item)=>(
-      <li key={`${props.gender}-${item}`}><Link reloadDocument className="button" to={`/${item}`}>{ item }</Link></li>
-    ))
-  )
+  if (props.gender === "all"){
+    return (
+      years.map((item)=>(
+        <li key={`${props.gender}-${item}`}><Link reloadDocument className="button" to={`/${item}`}>{ item }</Link></li>
+      ))
+    )
+  }else {
+    if (props.gender === "f"){
+      return (
+        years.map((item)=>(
+          <li key={`${props.gender}-${item}`}><Link reloadDocument className="button" to={`/${item}/girls`}>{ item }</Link></li>
+        ))
+      )
+    }
+    if (props.gender === "m"){
+      return (
+        years.map((item)=>(
+          <li key={`${props.gender}-${item}`}><Link reloadDocument className="button" to={`/${item}/boys`}>{ item }</Link></li>
+        ))
+      )
+    }
+  }
+  
 }
 
 export default YearLinks;
--- src/pages/gender/Gender.jsx
+++ src/pages/gender/Gender.jsx
@@ -55,16 +55,22 @@ function Gender() {
             <SplitTable names= { boys }/>
           </div>
           <div>
-            <h2>Top 100 Boys Names By Year</h2>
-            <YearLinks />
-          </div>
-          <div>
             <h2>Top 100 Baby Names By Year</h2>
-            <YearLinks />
+            <ul>
+              <YearLinks gender="all" />
+            </ul>
           </div>
           <div>
             <h2>Top 100 Girls Names By Year</h2>
-            <YearLinks />
+            <ul>
+              <YearLinks gender="f"/>
+            </ul>
+          </div>
+          <div>
+            <h2>Top 100 Boys Names By Year</h2>
+            <ul>
+              <YearLinks gender="m"/>
+            </ul>
           </div>
         </>
       );
@@ -78,16 +84,22 @@ function Gender() {
             <SplitTable names= { girls }/>
           </div>
           <div>
-            <h2>Top 100 Boys Names By Year</h2>
-            <YearLinks />
-          </div>
-          <div>
             <h2>Top 100 Baby Names By Year</h2>
-            <YearLinks />
+            <ul>
+              <YearLinks gender="all" />
+            </ul>
           </div>
           <div>
             <h2>Top 100 Girls Names By Year</h2>
-            <YearLinks />
+            <ul>
+              <YearLinks gender="f"/>
+            </ul>
+          </div>
+          <div>
+            <h2>Top 100 Boys Names By Year</h2>
+            <ul>
+              <YearLinks gender="m"/>
+            </ul>
           </div>
         </>
       );
