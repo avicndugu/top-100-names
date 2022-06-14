f8bdaeef1506bc654da4bea2565ca463c2e08269
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-06-14 18:11:25 +0300

    Cycle through the year pages

--- src/components/AllBottomLinks.jsx
+++ src/components/AllBottomLinks.jsx
@@ -2,10 +2,12 @@ import YearLinksPlaceholder from './YearLinksPlaceholder';
 import YearLinks from './YearLinks';
 import YearLinksPagination from './YearLinksPagination';
 import useFetch from '../functions/useFetch';
+import {useState} from 'react';
 
 const AllBottomLinks = () => {
 const [isDataLoading, data] = useFetch('https://avicndugu.github.io/names-api/allyears.json');
 
+const [pageNumber, setPageNumber] = useState(1);
 
   if (isDataLoading || !data){
     return( 
@@ -39,7 +41,15 @@ const [isDataLoading, data] = useFetch('https://avicndugu.github.io/names-api/al
   //  console.log(data.length);
     const numberofpages = Math.floor(data.length/9) + 1;
   //  const yearspage1=data.slice(0,9);
-  //  console.log(yearspage1);
+    
+  //  for (let n=1; n<=numberofpages; n++) {
+  //    const yearspage1=data.slice(0,n*9);
+  //    console.log(yearspage1);
+  //  }
+  // console.log(data.slice(0,pageNumber*9));
+
+// Cycle through the years per page
+  console.log(data.slice((pageNumber-1)*9,pageNumber*9));
 
     return(
       <> 
@@ -48,7 +58,26 @@ const [isDataLoading, data] = useFetch('https://avicndugu.github.io/names-api/al
           <div className="bottom-links">
             <YearLinks gender="all" yearslist={ data }/>
           </div>
-          <YearLinksPagination newer={ 1 } older={ 1 } pages={ numberofpages } />
+          <div className="pagination">
+            <button onClick={()=> {
+              if(pageNumber>1){
+                setPageNumber(pageNumber - 1);
+              }
+            }}>Newer</button>
+            <div>
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>4</button>
+              <span> ... </span>
+              <button className="pagination-number">{ numberofpages }</button>
+            </div>
+            <button onClick={()=> {
+              if (pageNumber<numberofpages){
+                setPageNumber(pageNumber + 1);
+              }
+            }}>Older</button>
+          </div>          
         </div>
         <div>
           <h2 className="text-center">Top 100 Girls Names By Year</h2>
