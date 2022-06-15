b5e9583e4fb6800f924ae505d5c9129c2fe44f94
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-06-15 15:21:28 +0300

    Seperate pagination for each section

--- src/components/AllBottomLinks.jsx
+++ src/components/AllBottomLinks.jsx
@@ -8,6 +8,8 @@ const AllBottomLinks = () => {
 const [isDataLoading, data] = useFetch('https://avicndugu.github.io/names-api/allyears.json');
 
 const [pageNumber, setPageNumber] = useState(1);
+const [pageNumberBoys, setPageNumberBoys] = useState(1);
+const [pageNumberGirls, setPageNumberGirls] = useState(1);
 
   if (isDataLoading || !data){
     return( 
@@ -36,6 +38,9 @@ const [pageNumber, setPageNumber] = useState(1);
     const numberofpages = Math.floor(data.length/9) + 1;
     // Cycle through the years per page
     let currentpageyears = data.slice((pageNumber-1)*9,pageNumber*9);
+    let currentpageyearsboys = data.slice((pageNumberBoys-1)*9,pageNumberBoys*9);
+    let currentpageyearsgirls = data.slice((pageNumberGirls-1)*9,pageNumberGirls*9);
+
     return(
       <> 
         <div>
@@ -67,16 +72,54 @@ const [pageNumber, setPageNumber] = useState(1);
         <div>
           <h2 className="text-center">Top 100 Girls Names By Year</h2>
           <div className="bottom-links">
-            <YearLinks gender="f" yearslist={ data }/>
+            <YearLinks gender="f" yearslist={ currentpageyearsgirls }/>
           </div>
-          <YearLinksPagination newer={ 1 } older={ 1 } pages={ numberofpages } />
+          <div className="pagination">
+            <button onClick={()=> {
+              if(pageNumberGirls>1){
+                setPageNumberGirls(pageNumberGirls - 1);
+              }
+            }}>Newer</button>
+            <div>
+              <button onClick={()=> setPageNumberGirls(1)}>1</button>
+              <button onClick={()=> setPageNumberGirls(2)}>2</button>
+              <button onClick={()=> setPageNumberGirls(3)}>3</button>
+              <button onClick={()=> setPageNumberGirls(4)}>4</button>
+              <span> ... </span>
+              <button className="pagination-number" onClick={()=> setPageNumber(numberofpages)}>{ numberofpages }</button>
+            </div>
+            <button onClick={()=> {
+              if (pageNumberGirls<numberofpages){
+                setPageNumberGirls(pageNumberGirls + 1);
+              }
+            }}>Older</button>
+          </div>
         </div>
         <div>
           <h2 className="text-center">Top 100 Boys Names By Year</h2>
           <div className="bottom-links">
-            <YearLinks gender="m" yearslist={ data }/>
+            <YearLinks gender="m" yearslist={ currentpageyearsboys }/>
            </div>
-           <YearLinksPagination  newer={ 1 } older={ 1 } pages={ numberofpages } />
+           <div className="pagination">
+            <button onClick={()=> {
+              if(pageNumberBoys>1){
+                setPageNumberBoys(pageNumberBoys - 1);
+              }
+            }}>Newer</button>
+            <div>
+              <button onClick={()=> setPageNumberBoys(1)}>1</button>
+              <button onClick={()=> setPageNumberBoys(2)}>2</button>
+              <button onClick={()=> setPageNumberBoys(3)}>3</button>
+              <button onClick={()=> setPageNumberBoys(4)}>4</button>
+              <span> ... </span>
+              <button className="pagination-number" onClick={()=> setPageNumberBoys(numberofpages)}>{ numberofpages }</button>
+            </div>
+            <button onClick={()=> {
+              if (pageNumberBoys<numberofpages){
+                setPageNumberBoys(pageNumberBoys + 1);
+              }
+            }}>Older</button>
+          </div>
         </div>
       </>
     )
