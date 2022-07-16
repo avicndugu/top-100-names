f5c9c11dfa8ca9c7bc825870cd8cd8664623254d
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-07-16 20:21:16 +0300

    Set up toggler with true false

--- src/components/ToggleFavourite.jsx
+++ src/components/ToggleFavourite.jsx
@@ -1,24 +1,40 @@
 import { useState } from 'react';
 
-const friendsnames = [{id: 1,name: "Agnes", favourite: "false"},{id: 2,name: "ALice", favourite: "false"},{id: 3,name: "Alvaro", favourite: "false"},{id: 4,name: "Arabia", favourite: "false"}];
+const friendsnames = [{id: 1,name: "Agnes", favourite: false},{id: 2,name: "ALice", favourite: false},{id: 3,name: "Alvaro", favourite: false},{id: 4,name: "Arabia", favourite: false}];
+
 
 const ToggleFavourite = () => {
   const [names, setNames] = useState(friendsnames);
-  
+
   // Function to set favourites
   const changeFavourite = (id) => {
     setNames(
       names.map((name) => {
           if(name.id == id){
-              return { ...name, favourite: "true" }
+            console.log(names)
+            return { ...name, favourite: !name.favourite }
           } else {
-              return{ ...name }
+            console.log(names)
+            return{ ...name }
           }
       })
     )
-    
   }
-
+  return (
+    <main>
+      <ul>
+        {
+          names.map((name) => (
+            <li key={name.id}> {name.id} { name.name }
+              <button onClick={()=>changeFavourite(name.id)}>
+                Toggle Favourite {name.favourite}
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+    </main>
+  )
 }
 
 
