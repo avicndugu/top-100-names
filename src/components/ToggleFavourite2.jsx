1bde9f53797e2388fbb58821e9e2133e7c169543
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-07-16 19:17:20 +0300

    Update a specific item

---
+++ src/components/ToggleFavourite2.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+// You have an array one of the arrays value will be mutable.
+const friendsArray = [
+    {
+        id: 1,
+        name: "John",
+        age: 19,
+    },
+    {
+        id: 2,
+        name: "Candy",
+        age: 18,
+    },
+    {
+        id: 3,
+        name: "mandy",
+        age: 20,
+    },
+];
+
+
+const ToggleFavourite2 = () => {
+    const [friends, setFriends] = useState(friendsArray); // Setting default value
+
+    // function to add new friend
+    const handleAddFriend = () => {
+        setFriends((prevFriends) => [
+            ...prevFriends,
+            {
+                name: "Random Friend Name",
+                age: 20, // Random age
+            },
+        ]);
+    };
+    // Function to edit values of one friend
+    const handleSecondFriend = () => {
+        setFriends(
+            friends.map((friend) =>{
+                // Here you accept a id argument to the function and replace it with hard coded 🤪 2, to make it dynamic.
+                if(friend.id === 2){
+                    return { ...friend, name: "Changed Name" }
+                } else {
+                    return { ...friend }
+                }
+            })
+        );
+    };
+
+
+
+    return (
+        <main>
+            <ul>
+                {/* Mapping over array of friends */} 
+                {friends.map((friend, index) => (
+                    // Setting "index" as key because name and age can be repeated, It will be better if you assign uniqe id as key
+                    <li key={index}>
+                        <span>name: {friend.name}</span>{" "}
+                        <span>age: {friend.age}</span>
+                    </li>
+                ))}
+                <button onClick={handleAddFriend}>Add Friends</button>
+
+                <button onClick={handleSecondFriend}>Change Second Name</button>
+
+            </ul>
+        </main>
+    );
+};
+
+export default ToggleFavourite2;
