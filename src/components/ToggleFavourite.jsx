2cbb30976f16927edbb2db30ec69a3949db6aa6f
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-07-16 16:56:07 +0300

    Pass state to button from function

--- src/components/ToggleFavourite.jsx
+++ src/components/ToggleFavourite.jsx
@@ -12,6 +12,39 @@ const ToggleFavourite = () => {
 
 function Board() {
   const [buttonsClicked, setButtonsClicked] = useState(Array(4).fill('Not Clicked'));
+  const myArray = [{name: "Agnes"},{name: "ALice"},{name: "Alvaro"},{name: "Arabia"}];
+//  const myNewArray = myArray.map(addStateToArray);
+
+  const [newButton, setNewButton] = useState(Array(4).fill('Unclicked'));
+ 
+  const changeButtonState = (buttonIndex)=> { 
+    const addAllButtonStatesToArray = myArray.map((item, index, array)=> {
+      array[index].newButton = newButton[index];
+      if(buttonIndex === index){
+        array[index].newButton= "Clicked";
+        return item;
+      } else {
+        return item;
+      }
+    });
+
+//    setNewButton(addAllButtonStatesToArray);
+    console.log(addAllButtonStatesToArray);
+  }
+// changeButtonState(0)
+ changeButtonState(1)
+
+
+//  function addStateToArray(item, index, array) {
+//    console.log(newButton)
+    //array[index].buttonsClicked = buttonsClicked[index];
+//    return array;
+//  };
+
+
+//  console.log(myNewArray);
+//  console.log(myArray);
+
   const buttonClick = (clickEvent, index) => {
     const clicks = buttonsClicked.map((clickvalue, i) => {
       if (i === index){
@@ -23,12 +56,20 @@ function Board() {
     });
     setButtonsClicked(clicks);
   }
+
+//  console.log(buttonsClicked);
   return (
     <>
       {
-      buttonsClicked.map((click, i)=>(
-        <button onClick={(clickEvent)=> buttonClick(clickEvent, i)}>Button {i}</button>
-      ))}
+        newButton.map( (item, index) =>(
+          <button onClick={()=>changeButtonState(index)}>LALA</button>
+        ))
+      }
+      {
+        buttonsClicked.map((click, i)=>(
+          <button onClick={(clickEvent)=> buttonClick(clickEvent, i)}>Button {i} {click}</button>
+        ))
+      }
     </>
   )
 }
