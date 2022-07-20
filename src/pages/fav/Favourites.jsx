6e7e40eafe445fa5e0e593a928df21a36ce328e3
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-07-20 17:38:21 +0300

    Display locally saved names

--- src/pages/fav/Favourites.jsx
+++ src/pages/fav/Favourites.jsx
@@ -3,28 +3,33 @@ import AllBottomLinks from '../../components/AllBottomLinks';
 
 
 function Favourites() {
-  if (true){
+  if (localStorage.key(0)!=='localfavourite'){
     return (
       <>
         <h1>Your Favourite Baby Names</h1>
         <div className="Row">
-          <div className="Column">
-            <PlaceHolder1To10 />
-          </div>
-          <div className="Column">
-            <PlaceHolder1To10 />
-          </div>
+          <p>No favourite baby names</p>
+          <br/>
+          <p>You have not choosen any baby names.</p>
+
+          <p>Once you like the baby names, they will appear here.</p>
         </div>
         <AllBottomLinks />
       </>
     )
   } else {
+    const currentlocalfav = JSON.parse(localStorage.getItem('localfavourite'));
+    console.log(currentlocalfav);
     return (
       <>
-        <h1>Your Favourite Baby Names</h1>
+        <h1>Your Favourite Baby Names data present</h1>
         <div className="Row">
           <div className="Column">
-            <PlaceHolder1To10 />
+            {
+              currentlocalfav.map((name)=>(
+                <p>{ name.name }</p>
+              ))
+            }
           </div>
           <div className="Column">
             <PlaceHolder1To10 />
