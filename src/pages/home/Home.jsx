4a59ab27923a410a9cbfad11676c82fab325d984
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-08-01 13:17:54 +0300

    Add favourites

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,8 @@ import PlaceHolder1To10 from '../../components/PlaceHolder1To10';
 import useFetch from '../../functions/useFetch';
 import { useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom';
+import createlocalstore from '../../functions/createlocalstore';
+import  changelocalstore from '../../functions/changelocalstore';
 
 function Home() {
   const [isDataLoading, data] = useFetch('https://jekyll-json-api.netlify.app/yob2021.json');
@@ -20,6 +22,9 @@ function Home() {
       setSeeAllBoys(location.state.resetviewall);
     },[resetstate]);
   }
+
+  const [localFav, setLocalFav] = useState(new Array(0));
+
 
   if (location.state !== null){
     UpdateViewAllState(location.state);
@@ -57,17 +62,28 @@ function Home() {
       </>
     )
   } else {
+
+    const namesList = data.map((item, index) => (
+      {
+        id: item.id,
+        name: item.name,
+        count: item.count,
+        gender: item.gender,
+        favourite: item.favourite
+      }
+    ));
+
     const newArrGirls= [];
     const newArrBoys= [];
 
-    const girls = data.filter((name) => name.gender === "F");
+    const girls = namesList.filter((name) => name.gender === "F");
     girls.forEach((item, index) =>{
       item.pos = index + 1;
       newArrGirls.push(item);
       return newArrGirls;
     })
 
-    const boys = data.filter((name) => name.gender === "M");
+    const boys = namesList.filter((name) => name.gender === "M");
     boys.forEach((item, index) =>{
       item.pos =  index + 1;
       newArrBoys.push(item);
@@ -78,6 +94,21 @@ function Home() {
     const top10boys=boys.filter(name => name.pos <= 10);
     const top100girls=girls.filter(name => name.pos <= 100);
     const top100boys=boys.filter(name => name.pos <= 100);
+
+
+    // Create an empty store if it does not exist else indicate the current user's favourite names
+    createlocalstore(namesList);
+
+
+
+    // Function to set favourites
+    const changeFavourite = (id) => {
+      // Change the favourite value of liked name
+      const selectedname = namesList.filter(name => name.id === id);
+      setLocalFav(
+        changelocalstore(localFav, id, selectedname)
+      )
+    }
 
     return (
       <>
@@ -90,7 +121,7 @@ function Home() {
             <h2>Top 100 Baby Boys Names in 2021</h2>
             <table>
               <tbody>
-                <TopNames names = { boys } top10 ={ top10boys } top100={ top100boys } gender = "m" seeall={ seeAllBoys } />
+                <TopNames names = { boys } top10 ={ top10boys } top100={ top100boys } gender = "m" seeall={ seeAllBoys } changeFavourite= {changeFavourite} />
               </tbody>
             </table>
             <div className="text-center">
@@ -101,7 +132,7 @@ function Home() {
             <h2>Top 100 Baby Girls Names in 2021</h2>
             <table>
               <tbody>
-                <TopNames names = { girls }  top10 ={ top10girls } top100={ top100girls } gender = "f" seeall={ seeAllGirls } />
+                <TopNames names = { girls }  top10 ={ top10girls } top100={ top100girls } gender = "f" seeall={ seeAllGirls } changeFavourite= {changeFavourite} />
               </tbody>
             </table>
             <div className="text-center">
