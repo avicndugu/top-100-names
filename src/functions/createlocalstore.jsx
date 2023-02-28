948808d2104c784ae11b6f087004fa1d9297d1e5
avicndugu <37213663+avicndugu@users.noreply.github.com>
2023-02-28 12:52:35 +0300

    Select users with matching names but different nameids

--- src/functions/createlocalstore.jsx
+++ src/functions/createlocalstore.jsx
@@ -5,8 +5,21 @@ function createlocalstore(namesList) {
     // Update state for names on the local database
     if (currentlocalfav.length > 0) {
       namesList.map((name) => {
+        // Indicate users favourite names based on matching ids
         currentlocalfav.filter((localname) => {
           if (name.id === localname.id) {
+            name.favourite = true;
+            return { ...name };
+          } else {
+            return { ...name };
+          }
+        });
+        return name;
+      });
+      // Add users favourite names based on matching names
+      namesList.map((name) => {
+        currentlocalfav.filter((localname) => {
+          if (name.name === localname.name) {
             name.favourite = true;
             return { ...name };
           } else {
