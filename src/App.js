0d715a2f8db16107eecd0928ec27f2cb2e445cd5
avicndugu <37213663+avicndugu@users.noreply.github.com>
2023-08-28 10:36:31 +0300

    Add icon to header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import logo from './logo.png';
+import heartsolid from './icons/heart-solid.png';
 import { Routes, Route} from 'react-router-dom';
 import Home from './pages/home/Home';
 import Year from './pages/year/Year';
 import Gender from './pages/gender/Gender';
 import Favourites from './pages/fav/Favourites';
 
-
 function App() {
   return (
     <div className="Container">
       <header className="App-header">
         <a href="/"><img src={logo} alt="Top 100 Names" height="50px"/></a>
-        <a href="/favourites">Favourite Names</a>
+        <a href="/favourites" style={{ display: 'flex', alignItems: 'center' }}>
+          <span>Saved</span>
+            <img style={{width: '30px', height: '30px', marginLeft: '10px'}} src={ heartsolid } alt={ "favourite names" } />
+        </a>
       </header>
       <main>
         <Routes>
